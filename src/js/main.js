392d03122d92b072c9483f3367748c1bb99b878d
Ksaltotun <romah28ius@gmail.com>
2019-04-23 19:06:26 +0300

    df

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,3 @@
 
-function random_number () {
-    return (Math.random() * (200) - 100);
-}
-function gen_mass (N){
-    var mass = [];
-    for (i = 0; i < N; i++){
-        mass[i] = random_number; 
-        
-    }
-    return mass;
-}
 
-no_sort_mass = gen_mass(5);
-function sort(no_sort_mass) {
-    var buf = 0, max = no_sort_mass[0];
-     
-    for (j = 0; j < no_sort_mass.length; j++ ){
-        for(i = j; i < no_sort_mass.length; i++){
-            if (max < no_sort_mass[i]){
-                buf = max;
-                max = no_sort_mass[i];
-                no_sort_mass = buf;
-            }
-
-
-    }
-    
-
-
-        
-    }
-
-
-}
 
